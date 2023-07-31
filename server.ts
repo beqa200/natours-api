413f810106cd7bd32a266c7884579debb5f38f74
beqa <beqamaisuradze912@gmail.com>
2023-07-31 18:37:46 +0400

    connect database

--- server.ts
+++ server.ts
@@ -2,15 +2,27 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './config.env' });
 
 import app from './app';
+import mongoose from 'mongoose';
 // console.log(process.env);
 
-if (process.env.DATABASE_PASSWORD) {
-  const DB = process.env.DATABASE_URL?.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
+if (!process.env.DATABASE_URL || !process.env.DATABASE_PASSWORD) {
+  throw new Error(
+    'Database URL or Database password is not defined in environment variables!'
   );
+}
+const DB_URL = process.env.DATABASE_URL?.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-  console.log(DB)
+try {
+  DB_URL &&
+    mongoose.connect(DB_URL).then((con) => {
+      console.log(con.connections);
+      console.log('Database connected!');
+    });
+} catch (error) {
+  console.error('Error connecting to database:', error);
 }
 
 const port = process.env.PORT || 3000;
