c64f20ec8b9c483aceb3bc30fb86455a165f83e2
beqa <beqamaisuradze912@gmail.com>
2023-08-04 02:06:24 +0400

    add filtering

--- controllers/tourController.ts
+++ controllers/tourController.ts
@@ -23,8 +23,17 @@ declare module 'express-serve-static-core' {
 // };
 const getAllTours = async (req: Request, res: Response) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -40,24 +49,21 @@ const getAllTours = async (req: Request, res: Response) => {
   }
 };
 
-const getTour = async(req: Request, res: Response) => {
+const getTour = async (req: Request, res: Response) => {
   try {
-    const tour = await Tour.findById(req.params.id)
+    const tour = await Tour.findById(req.params.id);
     res.status(200).json({
       status: 'success',
       data: {
         tour,
       },
     });
-  } catch(err) {
+  } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err
+      message: err,
     });
   }
-
-
- 
 };
 
 const createTour = async (req: Request, res: Response) => {
@@ -85,33 +91,32 @@ const updateTour = async (req: Request, res: Response) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true
-    })
+      runValidators: true,
+    });
     res.status(200).json({
       status: 'success',
       data: {
-        tour
+        tour,
       },
     });
-  }catch(err) {
+  } catch (err) {
     res.status(400).json({
       status: 'fail',
       message: err,
     });
   }
-
 };
 
 const deleteTour = async (req: Request, res: Response) => {
   try {
-    const tour = await Tour.findByIdAndDelete(req.params.id)
+    const tour = await Tour.findByIdAndDelete(req.params.id);
     res.status(200).json({
       status: 'success',
       data: {
-        tour
+        tour,
       },
     });
-  }catch(err) {
+  } catch (err) {
     res.status(400).json({
       status: 'fail',
       message: err,
--- server.ts
+++ server.ts
@@ -1,9 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './config.env' });
-
 import app from './app';
 import mongoose from 'mongoose';
-// console.log(process.env);
 
 if (!process.env.DATABASE_URL || !process.env.DATABASE_PASSWORD) {
   throw new Error(
@@ -18,7 +16,7 @@ const DB_URL = process.env.DATABASE_URL?.replace(
 try {
   DB_URL &&
     mongoose.connect(DB_URL).then((con) => {
-      console.log(con.connections);
+      // console.log(con.connections);
       console.log('Database connected!');
     });
 } catch (error) {
