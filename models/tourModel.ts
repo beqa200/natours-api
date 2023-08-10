3ca370fe53cbdbe03aeac45506b2108fa2c50095
beqa <beqamaisuradze912@gmail.com>
2023-08-11 03:08:22 +0400

    add custom validators

--- models/tourModel.ts
+++ models/tourModel.ts
@@ -43,7 +43,15 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Discount price should be below regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
