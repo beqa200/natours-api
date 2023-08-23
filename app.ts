720f732d2014c75b534fd84e25c9a23988d1f6d3
beqa <beqamaisuradze912@gmail.com>
2023-08-23 22:30:16 +0400

    add global error handling middleware

--- app.ts
+++ app.ts
@@ -3,6 +3,11 @@ import morgan from 'morgan';
 import tourRouter from './routes/tourRoutes';
 import userRouter from './routes/userRoutes';
 
+interface ErrorType extends Error {
+  status: string;
+  statusCode: number;
+}
+
 const app = express();
 
 // MIDDLEWARES
@@ -27,10 +32,23 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', (req: Request, res: Response) => {
-  res.status(404).json({
-    status: 'fails',
-    message: `Can't find ${req.originalUrl} on this server`,
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(
+    `Can't find ${req.originalUrl} on this server`
+  ) as ErrorType;
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err: ErrorType, req: Request, res: Response, next: NextFunction) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
