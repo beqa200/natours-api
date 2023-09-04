4e110c32d4fe2a8b477a726eaacdbbd31ac4ea2b
beqa <beqamaisuradze912@gmail.com>
2023-09-05 02:17:11 +0400

    give token to user

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,16 +1,26 @@
+import jwt from "jsonwebtoken";
 import { NextFunction, Response, Request } from 'express';
 import User from '../models/userModel';
 import catchAsync from '../utils/catchAsync';
+const signup = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
 
-const signup = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const newUser = await User.create(req.body);
+    const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN})
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      user: newUser,
-    },
-  });
-});
+    res.status(201).json({
+      status: 'success',
+      token,
+      data: {
+        user: newUser,
+      },
+    });
+  }
+);
 
 export { signup };
